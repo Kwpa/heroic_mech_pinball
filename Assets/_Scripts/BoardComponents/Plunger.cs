38b495c414f7cadfc98ccd32c768e29b58061b2b
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2025-11-15 09:17:31 -0400

    Add Basic Plunger
    Add the basic plunger functionality and add the component to the game environment.

---
+++ Assets/_Scripts/BoardComponents/Plunger.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Plunger : MonoBehaviour
+{
+    [Header("Controls")]
+    [SerializeField] private InputAction playerControls;
+
+    [Header("Settings")]
+    [SerializeField] private float launchForce = 2000;
+    [SerializeField] private float cooldownTime = 1;
+    private bool cooldown = false;
+
+    private List<Rigidbody> rigidbodies = new List<Rigidbody>();
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+        playerControls.performed += LaunchRigidBodies;
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+        playerControls.performed -= LaunchRigidBodies;
+    }
+
+    private void LaunchRigidBodies(InputAction.CallbackContext context)
+    {
+        Debug.Log("Input Detected");
+
+        if (cooldown) return; //exit clause 
+
+        foreach (Rigidbody rb in rigidbodies)
+        {
+            Debug.Log("Adding force to " + rb.gameObject.name);
+            rb.AddForce(Vector3.forward * launchForce, ForceMode.Impulse);
+        }
+
+        StartCoroutine(HandleCooldown());
+    }
+
+    private IEnumerator HandleCooldown()
+    {
+        cooldown = true;
+
+        yield return new WaitForSeconds(cooldownTime);
+
+        cooldown = false;
+    }
+
+    public void AddRbToList(Rigidbody rb)
+    {
+        rigidbodies.Add(rb);
+    }
+
+    public void RemoveRbFromList(Rigidbody rb)
+    {
+        rigidbodies.Remove(rb);
+    }
+}
